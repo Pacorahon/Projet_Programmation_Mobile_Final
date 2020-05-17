488eed74f6812a8675bb772d4458d0ad8c6c1558
Pacorahon <pacorahon@gmail.com>
2020-05-17 17:56:03 +0200

    stockage des données en cache

--- app/src/main/java/com/example/projetdevmobile/MainActivity.java
+++ app/src/main/java/com/example/projetdevmobile/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,12 +29,33 @@ public class MainActivity extends AppCompatActivity {
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private static final String BASE_URL = "https://raw.githubusercontent.com/Pacorahon/Projet_Programmation_Mobile_Final/master/app/src/main/java/com/example/projetdevmobile/";
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ShowList();
-        makeApiCall();
+        getSharedPreferences("app", Context.MODE_PRIVATE);
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        List<Character> characterList =getDataFromCache();
+        if(characterList != null){
+            ShowList(characterList)
+        } else {
+            makeApiCall();
+        }
+    }
+
+    private List<Character> getDataFromCache() {
+        String JsonCharacter = sharedPreferences.getString("JsonCharacterList",null);
+        if(JsonCharacter == null) {
+            return null
+        } else {
+            Type lisType = new TypeToken<List<Character>>() {}.getType();
+            return gson.fromJson(JsonCharacter, ListType);
+        }
     }
 
     private void ShowList(List<Character> characterList) {
@@ -44,9 +69,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void makeApiCall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -60,8 +82,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<RestRnMResponse> call, Response<RestRnMResponse> response) {
                 if(response.isSuccessful() && response.body() != null){
-                    List<Character> CharacterList = response.body().getResults();
-                    ShowList(CharacterList);
+                    List<Character> characterList = response.body().getResults();
+                    saveList(characterList);
+                    ShowList(characterList);
                 } else {
                     showError();
                 }
@@ -75,6 +98,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void saveList(List<Character> characterList) {
+        String JsonString = gson.toJson(characterList)
+        sharedPreferences
+                .edit()
+                .putString("JsonCharacterList",JsonString)
+                .apply();
+        Toast.makeText(getApplicationContext(),"Liste sauvegardée",Toast.LENGTH_SHORT).show();
     }
 
     private void showError() {
